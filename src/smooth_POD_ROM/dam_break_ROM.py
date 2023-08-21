7acb2cb1938ea8fc26342c076f468cc22a08a986
Florian Arbes <florian.arbes@t-online.de>
2023-08-21 13:02:30 +0200

    added exact deconvolution

--- src/smooth_POD_ROM/dam_break_ROM.py
+++ src/smooth_POD_ROM/dam_break_ROM.py
@@ -15,6 +15,7 @@ from smooth_POD_ROM.io import get_data, get_field
 from smooth_POD_ROM.plotting import plot_mesh, plot_field, plot_structured_field
 from copy import deepcopy
 from scipy.ndimage import gaussian_filter
+from scipy.fft import fft2, ifft2, fftshift, ifftshift
 from scipy.signal import wiener, convolve2d
 from skimage import restoration
 from skimage.restoration import richardson_lucy
@@ -97,7 +98,8 @@ def _find_largest_elements_2d(window, k=5):
 
 
 def deconvolve_exact(data_on_grid_smooth_s, psf, psf_f):
-    data_on_grid_smooth_f = to_frequency(data_on_grid_smooth_s, psf.shape)
+    # (151, 159), (57, 57), (151, 159)
+    data_on_grid_smooth_f = (fft2(data_on_grid_smooth_s).real)
     data_on_grid_f = data_on_grid_smooth_f / psf_f
     data_on_grid_s = to_space(data_on_grid_f)
     return data_on_grid_s
@@ -136,13 +138,23 @@ def local_mass_conservation(data, s):
 def crop(data_full, kernel):
     # psf = Point Spread Function
     px, py = kernel.shape
-    px = (px-1)//2
-    py = (py-1)//2
+    px = (px+1)//2
+    py = (py+1)//2
     return data_full[px:-px, py:-py]
 
 
 if __name__ == "__main__":
     # TODO fix error by figuring out all shapes flat and 2D. snapshots on cells, points, grid, smooth in space, frequency
+    # points: (9281, 3)
+    # triangles: (18144, 3)
+    # point_data: (9281,)
+    # data_on_grid: (93, 101)
+    # data_on_grid_f: (151, 159)
+    # data_on_grid_s: (151, 159)
+
+    # psf (Point Spread Function): (57, 57)
+    # psf_f (frequencies): (151, 159)
+
     sigma = 7
 
     file = "C:/Users/florianma/OneDrive - Institutt for Energiteknikk" + \
@@ -154,9 +166,15 @@ if __name__ == "__main__":
         points[:, :2],  point_data['alpha.water'])
     # data_on_grid_s = gaussian_filter(data_on_grid, sigma=sigma)
     psf = _gauss_2d(sigma, truncate=4, size=False)
+    # ------------------------------------------------------------------------
     psf_f = to_frequency(psf, data_on_grid.shape)
-    data_on_grid_s = convolve2d(data_on_grid, psf,
-                                boundary='fill', mode='full')
+    data_on_grid_f = to_frequency(data_on_grid, psf.shape)
+    data_on_grid_smooth_f = data_on_grid_f * psf_f
+    data_on_grid_s = to_space(data_on_grid_smooth_f)
+    # ------------------------------------------------------------------------
+    # data_on_grid_s = convolve2d(data_on_grid, psf,
+    #                             boundary='fill', mode='full')
+    # ------------------------------------------------------------------------
     # plotting:
     fig, ax = plt.subplots()
     plot_mesh(points, polys)
@@ -184,7 +202,7 @@ if __name__ == "__main__":
         m, n = snapshots.shape
         X_grid = np.empty((data_on_grid.size, n))
         # FIXME
-        X_grid_smooth = np.empty((24009, n))
+        X_grid_smooth = np.empty((data_on_grid_s.size, n))
         for j in range(n):
             print(j)
             data_on_grid = griddata(points[:, :2], snapshots[:, j], (X, Y))
@@ -211,20 +229,21 @@ if __name__ == "__main__":
     reg = RegularGrid()
     pod = POD()
     x_test = np.array([[500, 75, 30]])
+    # x_test = np.array([[100, 50, 30]])
     predictions = []
     for ss in (snapshots.T, X_grid.T, X_grid_smooth.T):
         print(p[:, :2].shape, ss.shape)
-        db = Database(p[:, :3], ss)
+        db = Database(p[:, :2], ss)
         rom = ROM(db, pod, reg)
         rom.fit()
-        predictions.append(rom.predict(x_test[:, :3]))
+        predictions.append(rom.predict(x_test[:, :2]))
 
     smoothROMprediction = predictions[2].copy().reshape(data_on_grid_s.shape)
     # res_pp = deconvolution_gauss(data)
     # res_pp = laplacian_deconvolution(data)
     # res_pp = wiener(res_pp, mysize=(11, 11))
     # res_pp = local_mass_conservation(data)
-    data_decon = richardson_lucy(smoothROMprediction, psf, num_iter=30)
+    # data_decon = richardson_lucy(smoothROMprediction, psf, num_iter=30)
     data_decon = deconvolve_exact(smoothROMprediction, psf, psf_f)
     data_decon2 = local_mass_conservation(data_decon, 20)
     data_decon3 = local_mass_conservation(data_decon2, 10)
